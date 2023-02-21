cc017f882a7fa0befeec8951ca240a803437b895
HossinAmin69 <hossinamin@diverse.dev>
2023-02-21 15:11:42 +0200

    creating card compoents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+const cardImages = [
+  { src: "/img/helment-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scrool-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
 function App() {
-  return <div className="bg-purple-400 h-[100vh]"></div>;
+  const [cards, setCards] = useState([]);
+
+  const suffleCards=()=>{
+    setCards()
+  }
+  return (
+    <div className="flex flex-col items-center bg-purple-400 h-[100vh]">
+      <h1 className="text-center m-0">Magic card Game</h1>
+      <button>Start New Game</button>
+
+      <div>
+        <img src={cardImages[1].src} alt="a card" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
