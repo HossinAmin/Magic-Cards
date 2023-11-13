37466d9d447fe84a65ee76b2297d0421dd8bc801
hossinamin <hossinamin70@gmail.com>
2023-11-13 13:38:13 +0200

    game functionalility

--- src/App.js
+++ src/App.js
@@ -1,27 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import SingleCard from "./components/SingleCard";
 const cardImages = [
-  { src: "/img/helment-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scrool-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
+  const [numTurns, setNumTurns] = useState(null);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const newGame = () => {
+    // suffles card deck
+    const suffledCards = [...cardImages, ...cardImages]
+      .map((card) => ({
+        id: Math.random(),
+        ...card,
+      }))
+      .sort(() => Math.random() - 0.5);
 
-  const suffleCards=()=>{
-    setCards()
-  }
+    setCards(suffledCards);
+
+    setNumTurns(0);
+  };
+  const handelChoice = (card) => {
+    // save selection
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  const compareCards = () => {
+    if (choiceOne.src === choiceTwo.src) {
+      // found a match
+      setCards(
+        cards.map((item) => {
+          if (item.src === choiceOne.src) {
+            item.matched = true;
+          }
+          return item;
+        })
+      );
+    }
+  };
+  const resetChoices = () => {
+    setTimeout(() => {
+      setChoiceOne(null);
+      setChoiceTwo(null);
+    }, 1000);
+
+    setNumTurns((prevState) => prevState + 1);
+  };
+
+  useEffect(() => {
+    // check two cards are selected
+    if (choiceOne && choiceTwo) {
+      compareCards();
+      resetChoices();
+    }
+  }, [choiceOne, choiceTwo]);
   return (
-    <div className="flex flex-col items-center bg-purple-400 h-[100vh]">
+    <div className="flex flex-col justify-start items-center bg-purple-900 h-screen">
       <h1 className="text-center m-0">Magic card Game</h1>
-      <button>Start New Game</button>
-
-      <div>
-        <img src={cardImages[1].src} alt="a card" />
+      <button onClick={newGame}>Start New Game</button>
+      <div className="card-deck flex flex-wrap gap-[25px] items-center w-[375px] my-[15px]">
+        {cards.map((card) => {
+          return (
+            <SingleCard
+              key={card.id}
+              card={card}
+              handelChoice={handelChoice}
+              flipped={card === choiceOne || card === choiceTwo || card.matched}
+              disabled={choiceOne && choiceTwo}
+            />
+          );
+        })}
       </div>
+      {numTurns != null && <p>{numTurns} Turns Made </p>}
     </div>
   );
 }
